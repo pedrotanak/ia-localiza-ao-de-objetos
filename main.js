52c7d95d17ca24a1b88bef575ed442c7cc7b9b98
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2023-05-11 17:12:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status="";
 video = "";
+objects = [];
 function preload() {
     video = createVideo('');
     video.hide();
@@ -10,6 +11,20 @@ function setup(){
 }
 function draw(){
     image(video,0,0,680,580);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.lenght; i++) {
+            document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+            document.getElementById("numberOfObject").innerHTML = "Quantidade de Objetos Detectados: " + objects.lenght;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd', modelLoaded);
@@ -21,4 +36,11 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
-}6
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
